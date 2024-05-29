62844984561dd887078fb7c618f605b58086eebb
GreatnessGrace <graceunstoppable7@gmail.com>
2024-05-29 16:41:36 +0530

    dist update

--- dist/index.js
+++ dist/index.js
@@ -8,7 +8,11 @@ const data_source_1 = require("./data-source");
 const contactRouter_1 = __importDefault(require("./router/contactRouter"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+const path_1 = __importDefault(require("path"));
 app.use('/', contactRouter_1.default);
+app.get('/', (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, 'index.html'));
+});
 data_source_1.AppDataSource.initialize().then(() => {
     app.listen(10000, () => {
         console.log('Server is running on port 3000');
