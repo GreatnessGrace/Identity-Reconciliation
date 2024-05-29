d2b32dffd931ac773f5c0f85eedd21f00fb4e88a
GreatnessGrace <graceunstoppable7@gmail.com>
2024-05-29 15:59:36 +0530

    db port update

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,11 +1,13 @@
 import { DataSource } from 'typeorm';
 import { Contact } from './entity/contact';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
+
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: process.env.HOST,
-  port: 3306,
+  port: 13236,
   username: process.env.USER_NAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
